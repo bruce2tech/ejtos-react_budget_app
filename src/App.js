f9c5da00dc6e5b068f8873536c14638c2543c594
bruce2tech <pbruce.resume@gmail.com>
2023-10-17 20:51:58 -0400

    Created basic budget component Using the useState hook to create a state variable called newBudget and initialize it with the current value of budget
    Defining a function called handleBudgetChange that updates the value of newBudget when the user changes the v
    alue of the input field
    
    Setting the value attribute of the input field to newBudget and adding an onChange event listener that calls
    handleBudgetChange when the user changes the value of the input field
    
    Using the Bootstrap Alert classes to give a nice gray background by adding some text and hard coding a value
    
    Made changes in App.js to display the  budget component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-import Remaining from './components/Remaining';
 
 // Add code to import the other components here under
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
-import AllocationForm from './components/AllocationForm'; 
+
+
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
@@ -20,44 +17,29 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
-                            <div className='col-sm'>
+                            <div className= 'col-sm'>
                                 <Budget />
                             </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
-                            <div className='col-sm'>
-                                <Remaining />
-                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
-                            <div className='col-sm'>
-                                <ExpenseTotal />
-                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
-                            <div className='col-sm'>
-                                <ExpenseList />
-                            </div>
                         }         
 
                         {
                             /* Add ExpenseItem component here */
-                            <div className='col-sm'>
-                                <ExpenseItem />
-                            </div>
                         }        
 
                         {
                             /* Add AllocationForm component here under */
-                            <div className='col-sm'>
-                                <AllocationForm />
-                            </div>
                         }        
 
                 </div>
